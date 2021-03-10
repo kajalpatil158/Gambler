a721b47c629acb1f2a52e8bd06e375d6cf29d4b3
Kajal <kajalpatil158@gmail.com>
2021-03-10 13:30:52 +0530

    [Kajal Patil]Added: Check_Condition_upto_Some_Value

--- Gambler/Gambler/Program.cs
+++ Gambler/Gambler/Program.cs
@@ -1,29 +1,45 @@
 ﻿using System;
 
-
-namespace Gambler
+namespace gambler
 {
     class Program
     {
+        //public static int End = 100;
         public static int Start = 1;
-        public static int End = 100;
-        public static int bet = 1;
-        public static int loss = 0;
-        static void Main(string[] args)
-       { 
-        
-        Random random = new Random();
-        int Check = random.Next(0, 2);
-            if (Check==bet)
-            {
-                Console.WriteLine("You are Won");
-            }
-            else
-            {
-                  Console.WriteLine("You are loss");
-            }
 
-           
+
+        static void Main(string[] args)
+        {
+
+
+            Condition();
+
+
+        }
+        public static void Condition()
+        {
+            int bet = 50;
+            int stack = 100;
+            //int Won = 100;
+            int loss = 50;
+            while (stack > loss && stack != loss)
+            {
+                Random random = new Random();
+                int Check = random.Next(0, 2);
+                if (Check == 1 && Start == 1)
+                {
+                    Console.WriteLine("you win");
+                    stack=stack+bet;
+                    Console.WriteLine("you left cost " + stack);
+                }
+                else
+                {
+                    Console.WriteLine("you lose");
+                    stack=stack-bet;
+                    Console.WriteLine("you left cost " + stack);
+                }
+                Console.ReadKey();
+            }
         }
     }
 }
