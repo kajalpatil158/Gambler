92da04b898ac5870593d14dbb70027a4877b0c59
Kajal <kajalpatil158@gmail.com>
2021-03-10 12:40:23 +0530

    [Kajal Patil]Added: Check_Condition_Loos_Or_Win

--- Gambler/Gambler/Program.cs
+++ Gambler/Gambler/Program.cs
@@ -1,15 +1,29 @@
 ﻿using System;
 
+
 namespace Gambler
 {
     class Program
     {
+        public static int Start = 1;
+        public static int End = 100;
+        public static int bet = 1;
+        public static int loss = 0;
         static void Main(string[] args)
-        {
-            int bet = 1;
-            int cash = 100;
-            Console.WriteLine("In Gambler i start with cash {0}", cash);
-             Console.WriteLine("I spend my cash for every game is: {0}", bet);
+       { 
+        
+        Random random = new Random();
+        int Check = random.Next(0, 2);
+            if (Check==bet)
+            {
+                Console.WriteLine("You are Won");
+            }
+            else
+            {
+                  Console.WriteLine("You are loss");
+            }
+
+           
         }
     }
 }
