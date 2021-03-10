4f21bc75a81aac352f36b924c5ee9cb253f6ad30
Kajal <kajalpatil158@gmail.com>
2021-03-10 11:45:17 +0530

    [Kajal Patil]Added UC1_Gambler_Start

---
+++ Gambler/Gambler/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Gambler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bet = 1;
+            int cash = 100;
+            Console.WriteLine("In Gambler i start with cash {0}", cash);
+             Console.WriteLine("I spend my cash for every game is: {0}", bet);
+        }
+    }
+}
